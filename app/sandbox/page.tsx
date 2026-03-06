099bdc2ad49c4f68fe18fd796020da0bea77ded8
Abhisharydv90 <140367934+Abhisharydv90@users.noreply.github.com>
2026-03-06 10:43:10 +0530

    initial commit from agencyos ipad matrix

---
+++ app/sandbox/page.tsx
@@ -0,0 +1,5 @@
+export default function BuildasimpleredComponent() {
+  return (
+    <div className="min-h-screen bg-[#020202] text-white p-8"><h1 className="text-emerald-400 text-2xl">System Online: Build a simple red login button component</h1></div>
+  )
+}
